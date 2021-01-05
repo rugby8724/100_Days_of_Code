3a86aea8ab51850ae85293e95e5b9e39b20ba715
Jeremiah <jeremiah.d.wise@gmail.com>
2021-01-05 14:18:47 -0600

    First Commit

---
+++ one/band-name-generator.py
@@ -0,0 +1,4 @@
+print('Welcome to the Band Name Generator.')
+street = input('What is the street name you grew up on?\n')
+pet = input('What is your pet\'s name?')
+print('Your band name could be ' + street + ' ' + pet + 's')
