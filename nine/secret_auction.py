027a25ada89266b780ff0eb5598e7d20cb82a5a4
Jeremiah <jeremiah.d.wise@gmail.com>
2021-01-13 11:45:27 -0600

    Add highest_bidder function

--- nine/secret_auction.py
+++ nine/secret_auction.py
@@ -1,15 +1,22 @@
 import os
-from art import logo as logo
-
-
+from art import logo
 
 
 print(logo)
 
 bids = {}
 
-new_bid = True
+def highest_bidder(bids):
+    bidder = ''
+    bid = 0
+    for k, v in bids.items():
+        if v > bid:
+            bidder = k
+            bid = v
+    print(f'The winning bidder is {bidder} with a bid of ${bid}')
 
+
+new_bid = True
 while new_bid == True:
     name = input('What is your name?  ')
     bid = int(input('What\'s your bid?  '))
@@ -19,14 +26,8 @@ while new_bid == True:
     new_bidder = input('Are there any other bidders? Type "y" or "n"  ').lower()
     if new_bidder == 'n':
         new_bid = False
+        os.system('clear')
+        highest_bidder(bids)
     else:
         os.system('clear')
         continue
-os.system('clear')
-bidder = ''
-bid = 0
-for k, v in bids.items():
-    if v > bid:
-        bidder = k
-        bid = v
-print(f'The winning bidder is {bidder} with a bid of {bid}')
