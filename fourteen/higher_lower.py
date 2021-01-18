880910ea346b9e7d229a537566a2e760430b2cf4
Jeremiah <jeremiah.d.wise@gmail.com>
2021-01-18 11:57:39 -0600

    Simplify insta_compare

--- fourteen/higher_lower.py
+++ fourteen/higher_lower.py
@@ -16,19 +16,20 @@ def new_insta(insta1=''):
 def insta_compare(insta1, insta2, pick):
     pick_1 = insta1['follower_count']
     pick_2 = insta2['follower_count']
-    if (pick_1 > pick_2 and pick == '1') or (pick_1 < pick_2 and pick == '2'):
-        return [True, 1]
+    if pick_1 > pick_2:
+        return pick == '1'
     else:
-        return [False, 0]
+        return pick == '2'
 
 
 def play_game():
-    
+
     insta_2 = new_insta()
-    count = 0
+    count = -1
     correct = True
     while correct == True:
         print(logo)
+        count += 1
         if count >= 1:
             print(f'You\'re right Current score: {count}')
         insta_1 = insta_2
@@ -40,8 +41,7 @@ def play_game():
 
         player_choice = input('Who has more followers? Press 1 or 2 ')
         compare_insta = insta_compare(insta_1, insta_2, player_choice)
-        correct = compare_insta[0]
-        count += compare_insta[1]
+        correct = compare_insta
         os.system('clear')
     print(f'Sorry you\'re wrong, you got {count} correct')
 
